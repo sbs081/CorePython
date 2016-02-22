dea6a43ea79d5dc5d17dc3acc23a967106665e20
sbs081 <aronivicki@gmail.com>
2016-02-22 13:42:41 +0800

    done

--- chapter02/example2-6.py
+++ chapter02/example2-6.py
@@ -1,19 +1,19 @@
+
 from socket import *
 from time import ctime
 
-HOST = ''
+HOST = 'localhost'
 PORT = 21567
-BUFSIZ = 1024
+BUFSIZE = 1024
 ADDR = (HOST, PORT)
 
 udpSerSock = socket(AF_INET, SOCK_DGRAM)
 udpSerSock.bind(ADDR)
 
 while True:
-    print('waiting for message...')
-    data, addr = udpSerSock.recvfrom(BUFSIZ).decode()
-    udpSerSock.send(('[%s]%s' %
-        (ctime(), data), addr).encode())
-    print('...received from and returned to:', addr)
+    print("waiting for message......")
+    data, addr = udpSerSock.recvfrom(BUFSIZE)
+    udpSerSock.sendto(('[%s] %s' % (ctime(), data)).encode(), addr)
+    print('......received from and returned to:', addr)
 
 udpSerSock.close()
--- chapter02/example2-7.py
+++ chapter02/example2-7.py
@@ -1,8 +1,8 @@
 from socket import *
-
+#不需要和服务器建立链接直接把消息发出去然后等待服务器的回复。
 HOST = 'localhost'
 PORT = 21567
-BUFSIZ = 1024
+BUFSIZE = 1024
 ADDR = (HOST, PORT)
 
 udpCliSock = socket(AF_INET, SOCK_DGRAM)
@@ -11,10 +11,10 @@ while True:
     data = input('> ')
     if not data:
         break
-    udpCliSock.send(data.encode(), ADDR).encode()
-    data, ADDR = udpCliSock.recv(BUFSIZ).decode()
+    data = data.encode()
+    udpCliSock.sendto(data, ADDR)
+    data, ADDR = udpCliSock.recvfrom(BUFSIZE)
     if not data:
         break
     print(data)
-
 udpCliSock.close()
