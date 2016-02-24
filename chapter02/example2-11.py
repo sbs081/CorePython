b52567456c97ba4a64ccdd72b712eb2820c1f8b4
sbs081 <aronivicki@gmail.com>
2016-02-24 10:05:49 +0800

    twisted reactor

---
+++ chapter02/example2-11.py
@@ -0,0 +1,28 @@
+from twisted.internet import protocol, reactor
+
+HOST = 'localhost'
+PORT = 21567
+
+class TSClntProtocol(protocol.Protocol):
+    def sendData(self):
+        data = input('> ')
+        if data:
+            print '...sending %s...' % data
+            self.transport.write(data)
+        else:
+            self.transport.loseConnection()
+
+    def connectionMade(self):
+        self.sendData()
+
+    def dataReceived(self, data):
+        print data
+        self.sendData()
+
+class TSClntFactory(protocol.ClientFactory):
+    protocol = TSClntProtocol
+    clientConnectionLost = clientConnectionFaild = \
+        lambda self, connector, reason: reactor.stop()
+
+reactor.connectTCP(HOST, PORT, TSClntFactory())
+reactor.run()
